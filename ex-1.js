7de2cdf41c75c7b2db3ccfd332eb44df07621862
Nat Tan <tresarpnat27@gmail.com>
2024-05-14 14:46:56 +0700

    resolv exercies function

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,14 @@
 // Start coding here
+function add(a, b) {
+  return a + b;
+}
+// console.log(add(2, 2));
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
